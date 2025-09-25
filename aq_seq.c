f645238368fe33a82d46d7233fea2d1f7770d7ce
CheatInNeed <peterroland02@gmail.com>
2025-09-25 11:51:28 +0200

    implemented some half done code :)

--- aq_seq.c
+++ aq_seq.c
@@ -7,12 +7,44 @@
 
 #include "aq.h"
 
+struct MsgNode {
+  void *payload;
+  struct MsgNode *next;
+};
+
+struct AlarmQueueStruct {
+  struct MsgNode *head;
+  struct MsgNode *tail;
+  bool hasAlarm;
+  void *alarm_payload;
+  int size = 0;
+};
+
 AlarmQueue aq_create( ) {
-  return NULL;
+  return (AlarmQueue)malloc(sizeof(struct AlarmQueueStruct));
 }
 
 int aq_send( AlarmQueue aq, void * msg, MsgKind k){
-  return AQ_NOT_IMPL;
+  if (msg == NULL) {
+    return AQ_NULL_MSG;
+  }
+  struct AlarmQueueStruct *queue = (struct AlarmQueueStruct *) aq;
+  if (k == AQ_ALARM && !queue->hasAlarm) {
+    queue->alarm_payload = msg;
+    queue->hasAlarm = true;
+  } else {
+    return AQ_NO_ROOM;
+  } else {
+    MsgNode *newNode = (struct MsgNode*)malloc(sizeof(struct MsgNode));
+    newNode->payload = msg;
+    newNode->next = NULL;
+    if (tail != NULL) { // hvis køen ikke er tom
+      tail->next = newNode;
+      aq.tail = newNode;
+    } else {
+      aq.head = newNode;
+      aq.tail = newNode;
+    }
 }
 
 int aq_recv( AlarmQueue aq, void * * msg) {
